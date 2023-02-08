627e17e205bd67904d1bc06077622f63d1d3a915
Jen Jenkins <pchealthtech@proton.me>
2023-02-07 17:28:17 -0800

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,11 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * @author Jen Jenkins
+ * @version 2023.02.07
+ */
 public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
