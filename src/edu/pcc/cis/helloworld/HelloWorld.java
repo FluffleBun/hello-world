d5baeea96a887523dded7b450a7eda1cb61cd145
Jen Jenkins <pchealthtech@proton.me>
2023-02-07 17:31:57 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * @author Jen Jenkins
  * @version 2023.02.07
@@ -7,5 +9,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
